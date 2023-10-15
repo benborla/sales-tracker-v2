ea169534965080c2d1ce48dde3e16ab984c47ca2
Ben Borla <iborla@melistechnology.com>
2023-10-15 10:45:17 +0800

    updated naming on login and dashboard

--- resources/views/vendor/nova/partials/logo.blade.php
+++ resources/views/vendor/nova/partials/logo.blade.php
@@ -1,1 +1,7 @@
+@if (request()->getPathInfo() === '/login')
+<h2 class="text-white">
+{{ is_main_store() ? 'Sales Tracker' : store()->name }}
+</h2>
+@else
 <h2 class="text-white">Sales Tracker</h2>
+@endif
